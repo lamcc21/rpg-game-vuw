03da157d4c35e2ce4c4a6cfe014d7e6282ce1eb3
mearslach <spr1ngy457>
2018-09-27 15:22:44 +1200

    Implemented drawSurface and drawObject skeletons

--- src/Renderer/CanvasPane.java
+++ src/Renderer/CanvasPane.java
@@ -2,12 +2,19 @@ package Renderer;
 
 import javax.swing.*;
 import java.awt.*;
+import GameWorld.*;
+
+import static GameWorld.GameWorld.Direction.*
 
 public class CanvasPane extends JPanel {
   private Color color = Color.darkGray;
+  private GameWorld gameWorld;
+  GameWorld.Direction perspective;
 
-  public CanvasPane() {
+  public CanvasPane(GameWorld gameWorld) {
     setSize(800,800);
+    this.gameWorld = gameWorld;
+    perspective =  = GameWorld.getPlayer().getPerspective();
     //TODO: rendering of canvas
   }
 
@@ -18,6 +25,46 @@ public class CanvasPane extends JPanel {
     if (color != null) {
       drawImage.setBackground(color);
       drawImage.drawRect(0, 0, 500, 500);
+      drawRoom(g);
     }
+  }
+
+  public void drawRoom(Graphics g){
+      drawSurfaces(g);
+      drawObjects(g);
+  }
+
+  public void drawSurfaces(Graphics g){
+      Wall backWall = null;
+      switch (perspective){
+          case NORTH:
+              backWall = Room.getWalls().get(SOUTH);
+              break;
+          case EAST:
+              backWall = Room.getWalls().get(WEST);
+              break;
+          case SOUTH:
+              backWall = Room.getWalls().get(NORTH);
+              break;
+          case WEST:
+              backWall = Room.getWalls().get(EAST);
+              break;
+      }
+      if(backWall.isVisible()){
+          //draw wall
+      }
+  }
+
+  public void drawObjects(Graphics g){
+      for(WorldObject object: gameWorld.getRoom()){
+        drawObject(object);
+      }
+  }
+
+  public void drawObject(WorldObject object){
+      int objectDistance = object.getDistance();
+      if(object.isVisible()) {
+          //draw object
+      }
   }
 }
