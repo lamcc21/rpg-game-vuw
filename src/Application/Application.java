1d473c6b8ed38ae2b0de714056888064e85060cf
Lachlan McCrae <Locky1353@users.noreply.github.com>
2018-10-01 12:13:28 +1300

    Gui updated so rendering can now properly be started.

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,7 +1,9 @@
 package Application;
+import GameWorld.GameWorld;
 import Renderer.CanvasPane;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicArrowButton;
 import java.awt.*;
 import java.io.IOException;
 
@@ -10,35 +12,56 @@ import java.io.IOException;
  */
 
 public class Application extends JFrame{
-  private boolean gameStarted;
+  private boolean gameStarted; //Will be used for start screen
   private final CanvasPane canvas;
   private final InventoryPane inventory;
+  private final GameWorld gameWorld;
 
   private Application() throws IOException {
-    super("UNNAMED GAME");
-    setLayout(new BorderLayout());
+    super("Escape the Room");
+    setLayout(new GridBagLayout());
     setResizable(false);
     setUIFont(new javax.swing.plaf.FontUIResource("Futuro",Font.BOLD,15));
-    this.canvas = new CanvasPane();
+
+    this.gameWorld = new GameWorld();
+    this.canvas = new CanvasPane(gameWorld);
     this.inventory = new InventoryPane();
+
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.anchor = GridBagConstraints.NORTH;
 
     Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
     this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
     //This centers every window displayed
 
-    getContentPane().add(canvas, BorderLayout.CENTER);
-    getContentPane().add(inventory, BorderLayout.SOUTH);
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    add(canvas,gbc);
+
+    gbc.fill = GridBagConstraints.VERTICAL;
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    add(new BasicArrowButton(BasicArrowButton.WEST, Color.GRAY, Color.BLACK, Color.BLACK, Color.BLACK),gbc);
+
+    gbc.gridx = 2;
+    gbc.gridy = 0;
+    add(new BasicArrowButton(BasicArrowButton.EAST, Color.GRAY, Color.BLACK, Color.BLACK, Color.BLACK),gbc);
+
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    add(inventory,gbc);
 
     pack();
-    setLocationRelativeTo(null);
+
     setVisible(true);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
     startListening();
   }
 
   private void startListening() {
     //TODO: This method will will listen for updates and make changes To inventory/canvas with respective calls
-
   }
 
   public static void main(String[] args) throws IOException { new Application();}
--- src/Renderer/CanvasPane.java
+++ src/Renderer/CanvasPane.java
@@ -1,70 +1,73 @@
 package Renderer;
 
+/**
+ * For SWEN225 Group Project - Lachlan Mears
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import GameWorld.*;
 
-import static GameWorld.GameWorld.Direction.*
+import static GameWorld.GameWorld.Direction.*;
 
 public class CanvasPane extends JPanel {
-  private Color color = Color.darkGray;
   private GameWorld gameWorld;
   GameWorld.Direction perspective;
 
   public CanvasPane(GameWorld gameWorld) {
-    setSize(800,800);
+    setPreferredSize(new Dimension(800,600));
     this.gameWorld = gameWorld;
-    perspective =  = GameWorld.getPlayer().getPerspective();
-    //TODO: rendering of canvas
+    //perspective = GameWorld.getPlayer().getPerspective();
   }
 
   @Override
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
-    Graphics2D drawImage = (Graphics2D) g;
-    if (color != null) {
-      drawImage.setBackground(color);
-      drawImage.drawRect(0, 0, 500, 500);
-      drawRoom(g);
-    }
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setColor(Color.BLACK);
+    g2d.drawRect(300, 150, 200,140);
+    g2d.drawLine(0,0,300,150);
+    g2d.drawLine(0,600,300,290);
+    g2d.drawLine(800,0,500,150);
+    g2d.drawLine(800,600,500,290);
   }
 
   public void drawRoom(Graphics g){
-      drawSurfaces(g);
-      drawObjects(g);
+    drawSurfaces(g);
+    drawObjects(g);
   }
 
-  public void drawSurfaces(Graphics g){
-      Wall backWall = null;
-      switch (perspective){
-          case NORTH:
-              backWall = Room.getWalls().get(SOUTH);
-              break;
-          case EAST:
-              backWall = Room.getWalls().get(WEST);
-              break;
-          case SOUTH:
-              backWall = Room.getWalls().get(NORTH);
-              break;
-          case WEST:
-              backWall = Room.getWalls().get(EAST);
-              break;
-      }
-      if(backWall.isVisible()){
-          //draw wall
-      }
+  private void drawSurfaces(Graphics g){
+    /*Wall backWall = null;
+    switch (perspective){
+      case NORTH:
+        backWall = Room.getWalls().get(SOUTH);
+        break;
+      case EAST:
+        backWall = Room.getWalls().get(WEST);
+        break;
+      case SOUTH:
+        backWall = Room.getWalls().get(NORTH);
+        break;
+      case WEST:
+        backWall = Room.getWalls().get(EAST);
+        break;
+    }
+    if(backWall.isVisible()){
+      //draw wall
+    }*/
   }
 
-  public void drawObjects(Graphics g){
-      for(WorldObject object: gameWorld.getRoom()){
-        drawObject(object);
-      }
+  private void drawObjects(Graphics g){
+    /*for(WorldObject object: gameWorld.getRoom()){
+      drawObject(object);
+    }*/
   }
 
-  public void drawObject(WorldObject object){
-      int objectDistance = object.getDistance();
-      if(object.isVisible()) {
-          //draw object
-      }
+  private void drawObject(WorldObject object){
+    /*int objectDistance = object.getDistance();
+    if(object.isVisible()) {
+      //draw object
+    }*/
   }
 }
