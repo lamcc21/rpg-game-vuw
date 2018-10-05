0113e7c624bdc8cf9ece521df94afce77c40b831
gooblie <michael42h@gmail.com>
2018-10-05 14:37:53 +1300

    Debug persistence even more

--- src/GameWorld/Door.java
+++ src/GameWorld/Door.java
@@ -1,7 +1,5 @@
 package GameWorld;
 
-import java.awt.Color;
-
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
--- src/Persistence/GameWorldFactory.java
+++ src/Persistence/GameWorldFactory.java
@@ -1,11 +1,11 @@
 package Persistence;
 
 import GameWorld.*;
+import GameWorld.Color;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GameWorldFactory {
@@ -14,12 +14,13 @@ public class GameWorldFactory {
         Map<GameWorld.Direction, ArrayList<WorldObject>> contents= new HashMap<>();
         Map<GameWorld.Direction, Wall> walls = new HashMap<>();
 
+        Color blue = new Color(0, 0, 255);
         Wall northWall = new Wall(true, GameWorld.Direction.NORTH);
         walls.put(GameWorld.Direction.NORTH, northWall);
-        Door eastDoor = new Door(false, Color.BLUE);
+        Door eastDoor = new Door(false, blue);
         Wall eastWall = new Wall(true, GameWorld.Direction.EAST, eastDoor);
         walls.put(GameWorld.Direction.EAST, eastWall);
-        Door southDoor = new Door(false, Color.BLUE);
+        Door southDoor = new Door(false, blue);
         Wall southWall = new Wall(true, GameWorld.Direction.SOUTH, southDoor);
         walls.put(GameWorld.Direction.SOUTH, southWall);
         Wall westWall = new Wall(true, GameWorld.Direction.WEST);
@@ -29,6 +30,7 @@ public class GameWorldFactory {
         Room rooms[][] = new Room[1][1];
         rooms[0][0] = room;
         Player player = new Player(0, 0);
+
 
         GameWorld game = new GameWorld(player, rooms);
 
