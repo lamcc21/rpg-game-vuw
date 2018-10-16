2135394171ec91de0b658916d037a120e0879b9b
mearslach <spr1ngy457>
2018-10-16 16:20:09 +1300

    implemented functionality for objects to be removed when clicked on. Updated door to extend WorldObject to allow for boundingbox mapping

--- src/GameWorld/Door.java
+++ src/GameWorld/Door.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class Door {
+public class Door extends WorldObject{
 
 	private boolean isLocked;
 	private GameColor gameColor;
